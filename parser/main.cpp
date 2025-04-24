3c487381aeae35b5fcd518d35813b3670fd34354
potolok86 <potolok86@gmail.com>
2025-04-24 22:44:55 +0400

    корректировка путей

--- parser/main.cpp
+++ parser/main.cpp
@@ -12,14 +12,14 @@ int main(int argc, char *argv[])
 {
     map<string, map<string,funcDescription*>*> mapInterfaces;
 
-    std::string path = "..\\..\\interfaces_obj\\";
+    std::string path = "../../interfaces_obj/";
     for (const auto & entry : filesystem::directory_iterator(path))
     {   // std::cout <<  << std::endl;
 
         string iterfaceName = entry.path().string();
         if(Parser(iterfaceName, &mapInterfaces))
         {
-            CreateAdapters( &mapInterfaces, "..\\..\\adapters" );
+            CreateAdapters( &mapInterfaces, "../../adapters" );
             cout<< "so happy!"<<endl;
         }
     }
