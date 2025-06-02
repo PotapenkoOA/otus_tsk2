82db69ac0abb98d2dd3d5ee55bc2675f4c87e7f0
PotapenkoOA <potolok86@gmail.com>
2025-06-02 19:44:23 +0000

    Delete out/build/GCC 13.3.0 x86_64-linux-gnu directory

--- out/build/GCC 13.3.0 x86_64-linux-gnu/googletest/googlemock/CTestTestfile.cmake
+++
@@ -1,7 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /home/vboxuser/Documents/microservices/otus_3tsk/googletest/googlemock
-# Build directory: /home/vboxuser/Documents/microservices/otus_3tsk/out/build/GCC 13.3.0 x86_64-linux-gnu/googletest/googlemock
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
-subdirs("../googletest")
--- out/build/GCC 13.3.0 x86_64-linux-gnu/googletest/googletest/CTestTestfile.cmake
+++
@@ -1,6 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /home/vboxuser/Documents/microservices/otus_3tsk/googletest/googletest
-# Build directory: /home/vboxuser/Documents/microservices/otus_3tsk/out/build/GCC 13.3.0 x86_64-linux-gnu/googletest/googletest
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
